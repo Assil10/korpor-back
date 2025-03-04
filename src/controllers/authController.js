6c275ca4d72b79fee6e98700fb7528eb1f703924
Assil10 <khaldi.assil40@gmail.com>
2025-03-05 00:08:33 +1300

    updates

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -130,7 +130,7 @@ exports.signIn = async (req, res) => {
     const token = jwt.sign(
       { userId: user._id, email: user.email, role: user.role },
       process.env.JWT_SECRET, // Use a strong secret key from .env
-      { expiresIn: "4h" } // Token valid for 7 days
+      { expiresIn: "4h" } // Token valid for 4 hours
     );
 
     res.json({
